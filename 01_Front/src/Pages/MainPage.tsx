1700ea4a9303479a244244569d4dc6c95331d3f3
tottorisnow33 <tottorisnow33@outlook.com>
2022-05-14 11:51:32 +0900

    Reactプロジェクト用意

---
+++ 01_Front/src/Pages/MainPage.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import '../App.css';
+
+function MainPage() {
+  
+  const handleClick = ()=>{
+    console.log("test")
+  }
+
+
+  return (
+    <div className="Main">
+      <header className="Main-header">
+        test
+      </header>
+    </div>
+  );
+}
+
+export default MainPage;
